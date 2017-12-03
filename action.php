c67f146da61e8bf7c81de0015e1fe4db9a95c5d5
Sindr0me <kupriyanov.vo@gmail.com>
2017-12-03 22:05:52 +0300

    action.php

--- action.php
+++ action.php
@@ -9,9 +9,10 @@ print_r($json);
 foreach( $json['weather'] as $arr) {
     $weather = $arr['main'];
 }
-foreach ($json['main'] as $varray) {
-    $temp = $varray['temp'];
-}
+// foreach ($json['main'] as $varray) {
+//     $temp = $varray['temp'];
+// }
+$temp = $json['main']['temp']; 
 
 echo "<h1>Город: $json[name]</br>Погода: $weather<br>Температура: $temp</h1>";
 
